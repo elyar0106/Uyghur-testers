843b72f08791cc23e6f3a1916698e67bc5c6a2e9
“xiayimard <karwanghana@gmail.com>
2020-05-13 23:28:30 +0100

    project

---
+++ xahimardan java program/src/assiagnment14/Attstest.java
@@ -0,0 +1,18 @@
+package assiagnment14;
+
+public class Attstest {
+
+	public static void main(String[] args) {
+		
+
+		 Atts a = new Atts();
+		 
+		 a.name = "table";
+	     a.color = "brown";
+	     a.amount = 1;
+
+	     System.out.println(a.toString());
+
+	}
+
+}
